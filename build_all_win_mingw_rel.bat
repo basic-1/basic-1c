14a225f09870ee6fc3c21cc35b8228fa44e49525
basic-1 <b1justomore@gmail.com>
2023-10-11 22:03:01 +0300

    distribution packages are built

--- build_all_win_mingw_rel.bat
+++ build_all_win_mingw_rel.bat
@@ -1,23 +1,23 @@
 cd a1stm8\build
-call a1stm8_win_x64_mingw_rel.bat
+call a1stm8_win_x64_mingw_rel.bat %1
 cd ..\..
 
 cd b1c\build
-call b1c_win_x64_mingw_rel.bat
+call b1c_win_x64_mingw_rel.bat %1
 cd ..\..
 
 cd c1stm8\build
-call c1stm8_win_x64_mingw_rel.bat
+call c1stm8_win_x64_mingw_rel.bat %1
 cd ..\..
 
 cd a1stm8\build
-call a1stm8_win_x86_mingw_rel.bat
+call a1stm8_win_x86_mingw_rel.bat %1
 cd ..\..
 
 cd b1c\build
-call b1c_win_x86_mingw_rel.bat
+call b1c_win_x86_mingw_rel.bat %1
 cd ..\..
 
 cd c1stm8\build
-call c1stm8_win_x86_mingw_rel.bat
+call c1stm8_win_x86_mingw_rel.bat %1
 cd ..\..
--- common/source/gitrev.h
+++ common/source/gitrev.h
@@ -1,1 +1,1 @@
-#define B1_GIT_REVISION "11"
+#define B1_GIT_REVISION "12"
